e341e7ff2c0f79a3ca8d634ba0baa02878528e2b
5igerson <80487098+5igerson@users.noreply.github.com>
2021-03-23 14:03:19 +0100

    zrobiony program zadanie1 z SDIZO i pierwsze zadanie z IK

--- PUS/zaj1.m
+++ PUS/zaj1.m
@@ -11,5 +11,79 @@ Ks2=QgN/(3*(T1N-TzewN)+T2N-TzewN)
 Ks1=3*Ks2
 Ks0=(Ks2*(T2N-TzewN))/(T1N-T2N)
 
-Tw2=(Ks0*T1N+Ks2*Tzew)/(Ks0+Ks2)
-Tw1=
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     Tw1     %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+
+%Tzew jako zmienna
+for QgN=15000:5000:25000
+    %figure;
+    
+    i=1
+    for TzewN=-40:0.1:40
+   
+        Tw1(i)=(QgN+Ks1*TzewN+(Ks0*Ks2*TzewN)/(Ks2+Ks0))/(Ks1+Ks0-(Ks0)^2/(Ks2+Ks0));
+        i=i+1;
+    end
+    
+    plot(-40:0.1:40,Tw1);
+        xlabel('T zew');
+    ylabel('Tw1');
+    hold on
+end
+
+figure;
+%Q jako zmienna
+for TzewN=-30:10:10
+    %figure;
+    
+    i=1
+    for QgN=-40000:100:40000
+   
+        Tw1(i)=(QgN+Ks1*TzewN+(Ks0*Ks2*TzewN)/(Ks2+Ks0))/(Ks1+Ks0-(Ks0)^2/(Ks2+Ks0));
+        i=i+1;
+    end
+    
+    plot(-40:0.1:40,Tw1);
+    xlabel('Qg');
+    ylabel('Tw1');
+    hold on
+end
+
+%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%     Tw2      %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
+figure;
+%TzewN jako zmienna
+for QgN=15000:5000:25000
+    %figure;
+    
+    i=1
+    for TzewN=-40:0.1:40
+   
+        Tw2(i)=(QgN*Ks0+Ks1*Ks2*TzewN+Ks1*Ks0*TzewN+Ks0*Ks2*TzewN)/(Ks1*Ks2+Ks1*Ks0+Ks0*Ks2);
+        i=i+1;
+    end
+    
+    plot(-40:0.1:40,Tw2);
+    xlabel('T zew');
+    ylabel('Tw2');
+    hold on
+end
+
+
+figure;
+%Q jako zmienna
+for TzewN=-30:10:10
+    
+    
+    i=1
+    for QgN=-40000:100:40000
+   
+        Tw2(i)=(QgN*Ks0+Ks1*Ks2*TzewN+Ks1*Ks0*TzewN+Ks0*Ks2*TzewN)/(Ks1*Ks2+Ks1*Ks0+Ks0*Ks2);
+        i=i+1;
+    end
+    
+    plot(-40:0.1:40,Tw2);
+    xlabel('Qg');
+    ylabel('Tw2');
+    hold on
+end
+
--- SDIZO/zadanie1.py
+++ SDIZO/zadanie1.py
@@ -2,60 +2,178 @@ import numpy as np
 import configparser
 import pandas as pd
 import random
+from time import time
 import timeit
 import os
+import math
 
 clear = lambda: os.system('cls')
 clear()
 
+
+def quick_sort(dane):
+    dlugosc=len(dane)
+    if dlugosc <= 1:
+        return dane
+    else:
+        pivot=dane.pop()
+
+    wiekszy=[]
+    mniejszy=[]
+    for i in dane:
+        if i >pivot:
+            wiekszy.append(i)
+        else:
+            mniejszy.append(i)
+
+    return quick_sort(mniejszy)+[pivot]+quick_sort(wiekszy)
 #----------------------------------------------
-def calculate_menu(dane):
+def policz_srednia(dane):
 
-    print("1 - oblicz srednia arytmetyczna")
-    print("2 - oblicz wartosc minimalna oraz maksymalna")
-    print("3 - oblicz mediane")
-    print("0 - jednak nic")
+    suma=0
+    for i in range(len(dane)):
+        suma+=dane[i]
 
-    opcja_liczenia=int(input())
+    srednia=suma/len(dane)
+
+    return srednia
+#----------------------------------------------
+def gdzie_jest_min(dane):
+    najmniejsza=dane[0]
+    for i in range(len(dane)):
+        
+        if dane[i]<najmniejsza:
+            najmniejsza=dane[i]
+
+    return najmniejsza
+#----------------------------------------------
+def gdzie_jest_max(dane):
+    najwieksza=dane[0]
+    for i in range(len(dane)):
+        
+        if dane[i]>najwieksza:
+            najwieksza=dane[i]
+
+    return najwieksza
+#----------------------------------------------
+def policz_mediana(dane):
+    length=(len(dane)-1)/2
+    if len(dane)%2==0:
+        mediana=(dane[math.ceil(length)]+dane[int(length)])/2
+    elif len(dane)%2==1:
+        mediana=dane[int(length)]
+    else:
+        print("cos nie tak")
+
+
+    return mediana
+#----------------------------------------------
+def calculate_menu(dane,wyjscie,czasy,wejscie1,n):
+
+    print("Oblicz:")
+    print("1-srednia arytmetyczna")
+    print("2-wartosc min")
+    print("3-wartosc max")
+    print("4-mediana")
+    operacja=int(input("Twoj wybor: "))
     clear()
+    dfdict={}
+    if operacja==1:
 
-    if opcja_liczenia==1:
-        start = timeit.timeit()
-        print("srednia artymetyczna: ",np.mean(dane))
-        end = timeit.timeit()
-        print("czas srednia arytmetyczna",(end - start)*1000,"ms")
-    elif opcja_liczenia==2:
-        start = timeit.timeit()
-        print("min: ",min(dane))
-        end = timeit.timeit()
-        print("czas min",(end - start)*1000,"ms")
+        #liczenie sredniej arytmetycznej
+        start = time()
+        srednia_arytm=float(policz_srednia(dane))
+        print("srednia artymetycznad: ",srednia_arytm)
+        end = time()
+        czas_srednia_arytm=(end - start)*1000
+        print("czas srednia arytmetyczna: %.4f ms" %czas_srednia_arytm)
+        dfdict["co liczone"]=["wartosc srednia arytmetyczna"]
+        dfdict["wynik co liczone"]=[srednia_arytm]
+        dfdict["napis ilosc instancji"]=["ilosc instancji"]
+        dfdict["ilosc instancji"]=[n]
+        dfdict["napis czas wykonania"]=["czas wykonania"]
+        dfdict["czas wykonania"]=[czas_srednia_arytm]
+        df=pd.DataFrame(dfdict)
+        df.to_csv(wyjscie,sep=";",header=False,index=False,mode='a',decimal=",")
+        print("zapisano dane do pliku: ",wyjscie)
 
-        start = timeit.timeit()
-        print("max: ",max(dane))
-        end = timeit.timeit()
-        print("czas max",(end - start)*1000,"ms")
-    elif opcja_liczenia==3:
-        start = timeit.timeit()
-        print("mediana: ",np.median(dane))
-        end = timeit.timeit()
-        print("czas mediana: ",(end - start)*1000,"ms")
-    elif opcja_liczenia==0:
-        return
 
+    elif operacja==2:
+        #liczenie wartosci min
+        start = time()
+        wartosc_min=float(gdzie_jest_min(dane))
+        print("min: ",wartosc_min)
+        end = time()
+        czas_min=(end - start)*1000
+        print("czas min: %.4f ms" %czas_min)
+        dfdict["co liczone"]=["wartosc min"]
+        dfdict["wynik co liczone"]=[wartosc_min]
+        dfdict["napis ilosc instancji"]=["ilosc instancji"]
+        dfdict["ilosc instancji"]=[n]
+        dfdict["napis czas wykonania"]=["czas wykonania"]
+        dfdict["czas wykonania"]=[czas_min]
+        df=pd.DataFrame(dfdict)
+        df.to_csv(wyjscie,sep=";",header=False,index=False,mode='a',decimal=",")
+        print("zapisano dane do pliku: ",wyjscie)
+
+    elif operacja==3:
+        #liczenie wartosci max
+        start = time()
+        wartosc_max=float(gdzie_jest_max(dane))
+        print("max: ",wartosc_max)
+        end = time()
+        czas_max=(end - start)*1000
+        print("czas max: %.4f ms" %czas_max)
+        dfdict["co liczone"]=["wartosc max"]
+        dfdict["wynik co liczone"]=[wartosc_max]
+        dfdict["napis ilosc instancji"]=["ilosc instancji"]
+        dfdict["ilosc instancji"]=[n]
+        dfdict["napis czas wykonania"]=["czas wykonania"]
+        dfdict["czas wykonania"]=[czas_max]
+        df=pd.DataFrame(dfdict)
+        df.to_csv(wyjscie,sep=";",header=False,index=False,mode='a',decimal=",")
+        print("zapisano dane do pliku: ",wyjscie)
+
+    elif operacja==4:
+        #liczenie mediany
+        start = time()
+        dane_posortowane=quick_sort(dane.tolist())
+        wartosc_mediana=float(policz_mediana(np.array(dane_posortowane)))
+        print("mediana: ",wartosc_mediana)
+        end = time()
+        czas_mediana=(end - start)*1000
+        print("czas mediany: %.4f ms" %czas_mediana)
+        dfdict["co liczone"]=["wartosc mediana"]
+        dfdict["wynik co liczone"]=[wartosc_mediana]
+        dfdict["napis ilosc instancji"]=["ilosc instancji"]
+        dfdict["ilosc instancji"]=[str(n)]
+        dfdict["napis czas wykonania"]=["czas wykonania"]
+        dfdict["czas wykonania"]=[str(czas_mediana)]
+        df=pd.DataFrame(dfdict)
+        df.to_csv(wyjscie,sep=";",header=False,index=False,mode='a',decimal=",")
+        print("zapisano dane do pliku: ",wyjscie)
+        dane=np.append(dane,wartosc_mediana)
+        dane_posortowane=sorted(dane.tolist())
+        dane_posortowane=pd.DataFrame(dane_posortowane)
+        dane_posortowane.to_csv(wejscie1,index=False)
+        print("dodano mediane do pliku: ",wejscie1)
+
+    wybor=9
+    return wybor
 #----------------------------------------------
-def read_data(wejscie):
-
+def read_data(wejscie,n):
+    n=int(n)
     dane=pd.read_csv(wejscie)
     dane=pd.DataFrame.to_numpy(dane)
     print("Wczytano dane z pliku .csv")
-    return dane
+    return dane[0:n]
 #----------------------------------------------
 def generate_data(min,max,n):
     dane=np.zeros((n,1))
     for i in range(n):
         dane[i]=random.randint(min,max)
 
-    print(dane)
+    print("wygenerowano dane")
     return dane
 #----------------------------------------------
 def save_data(dane,wejscie):
@@ -63,61 +181,62 @@ def save_data(dane,wejscie):
     dane=pd.DataFrame(dane)
     dane.to_csv(wejscie,index=False)
     print("zapisano dane do pliku: ",wejscie)
-
 #----------------------------------------------
 def initialize():
     config = configparser.ConfigParser()
+    clear()
+    inicjujacy=input("Podaj nazwe pliku inicjujacego: ")
     try:
-        config.read(r'SDIZO\init.ini')
+        config.read(inicjujacy.lower()+".ini")
     except:
-        print("cos nie dziala")
+        print("nie otworzono pliku ini")
     else:
         pass
 
     print("sekcje w pliku init: ",config.sections())
 
-    min1=config.getint('parameters','min')
-    max1=config.getint('parameters','max')
-    ilosc_liczb=config.getint('parameters','ilosc_liczb')
+    min1=-1000000
+    max1=1000000
+    ilosc_liczb=10000000
     wejscie=config.get('path','dane')
+    wejscie1=config.get('path','dane1')
     wyjscie=config.get('path','wyjsciowe')
+    n=config.get('variables','n')
 
-    return min1,max1,ilosc_liczb,wejscie,wyjscie
-
+    return min1,max1,ilosc_liczb,wejscie,wyjscie,n,wejscie1
+#----------------------------------------------
 def menu():
 
     print("\n\n1 - wczytaj plik inicjujacy")
     print("2 - generuj dane")
     print("3 - zapisz dane")
     print("4 - wczytaj dane")
-    print("5 - Przejdz do menu obliczen - obliczenia korzystajac z gotowych funkcji")
-    print("6 - Przejdz do menu obliczen - obliczenia korzystajac z funkcji wlasnych")
+    print("5 - Przejdz do menu obliczen")
     print("0 - wyjscie z programu")
     
-    wybor=int(input())
+    wybor=int(input("Twoj wybor: "))
 
     return wybor
 #-----------------------------------------------
 
 while(True):
+    wybor=9
     wybor=menu()
     clear()
     if wybor==1:
-        [min1,max1,ilosc_liczb,wejscie,wyjscie]=initialize()
+        min1,max1,ilosc_liczb,wejscie,wyjscie,n,wejscie1=initialize()
+        
     elif wybor==2:
         dane=generate_data(min1,max1,ilosc_liczb)
     elif wybor==3:
         save_data(dane,wejscie)
     elif wybor==4:
-        dane=read_data(wejscie)
+        dane=read_data(wejscie,n)
     elif wybor==5:
-        calculate_menu(dane)
-    elif wybor==6:
-        print("tu bedzie menu gotowych funkcji")
+        wybor=calculate_menu(dane,wyjscie,n,wejscie1,n)
     elif wybor==0:
         break
-    else:
-        print("wybrano niepoprawna opcje!")
+
 
 
 
